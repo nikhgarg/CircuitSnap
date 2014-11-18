eab27b8bdbb2b4d67fdd00e7e398f9cc8a9ea023
Nikhil Garg <nikhil_garg@outlook.com>
2014-11-18 15:24:54 -0600

    added more support for images, fft as feature

--- test_contourmachinelearning.py
+++ test_contourmachinelearning.py
@@ -13,7 +13,16 @@ SIZESMALLSAVED = 20;
 ## need to load from all images/datasets instead of just one dataset
 samples = None;
 responses = None;
-featurestype = "nothreshold/";
+isPhoto = False;
+
+imgstring = "1";
+if isPhoto:
+    imgtype = ".jpg";   
+    featurestype = "photo/";
+else:
+    imgtype = ".PNG";
+    featurestype = "fft/";
+    
 dirtocheck = "./trainingdata/"+featurestype;
 responsespath = "responses";
 samplespath = "samples";
@@ -36,36 +45,51 @@ for root, _, files in os.walk(dirtocheck + samplespath):
             samples = [newSamples];
         else:
             samples = np.append(samples, [newSamples]);
+if isPhoto: #I honestly have no idea why samples shape is different for photos.
+    samples = samples[0];
+samples = samples[0];
 samples = samples.reshape(np.size(responses),np.size(samples)/np.size(responses)); 
-
+print samples
+print np.size(samples);
 model = cv2.KNearest()
 model.train(samples,responses)
 
 ############################# testing part  #########################
 
-imgstring = "12";
-imgtype = ".PNG";
 im = cv2.imread("images/" + imgstring + imgtype,cv2.CV_LOAD_IMAGE_COLOR)
+print "images/" + imgstring + imgtype;
+print np.size(im)
+resized = []
+if isPhoto:
+    im = cv2.resize(im, dsize = (0,0), fx = .1, fy = .1, interpolation = cv2.INTER_CUBIC);
 im2 = im.copy();
 out = np.zeros(im.shape,np.uint8)
 gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
-
+if isPhoto:
+    gray = cv2.GaussianBlur(gray,(5,5),0)
+thresh = cv2.adaptiveThreshold(gray,255,adaptiveMethod = cv2.ADAPTIVE_THRESH_GAUSSIAN_C,thresholdType = cv2.THRESH_BINARY,blockSize = 11, C=2)
+#if isPhoto:
+#    gray = thresh
 contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
 print responses
 for cnt in contours:
-    if cv2.contourArea(cnt)>1:
+    if cv2.contourArea(cnt)>15:
         [x,y,w,h] = cv2.boundingRect(cnt)
         cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
         roi = gray[y:y+h,x:x+w]
         roismall = cv2.resize(roi,(SIZESMALLSAVED,SIZESMALLSAVED))
         roismall = roismall.reshape((1,SIZESMALLSAVED*SIZESMALLSAVED))
         roismall = np.float32(roismall)
-        retval, results, neigh_resp, dists = model.find_nearest(roismall, k = 1)
+        regionfft = abs(np.fft.fft2(roismall))
+        fftsample = regionfft.reshape((1, SIZESMALLSAVED*SIZESMALLSAVED));
+        sample = np.float32([np.append(roismall,fftsample[0])]);
+        print sample
+        print np.size(sample)
+        retval, results, neigh_resp, dists = model.find_nearest(sample, k = 1)
         string = str(chr((results[0][0])))
         cv2.putText(out,string,(x,y+h),0,1,(0,255,0))
-cv2.imshow('orig', im2);
+cv2.imshow('orig', gray);
 cv2.imshow('im',im)
 cv2.imshow('out',out)
 cv2.waitKey(0)
