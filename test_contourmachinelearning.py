fb28a89f62ff86b782274861afef1ebd37025082
Nikhil Garg <nikhgarg93@gmail.com>
2014-11-22 16:37:41 -0600

    small

--- test_contourmachinelearning.py
+++ test_contourmachinelearning.py
@@ -43,19 +43,30 @@ model.train(samples,responses)
 
 ############################# testing part  #########################
 
-imgstring = "12";
-imgtype = ".PNG";
+isPhoto = False;
+
+imgstring = "sadiku5";
+if isPhoto:
+    imgtype = ".jpg";   
+else:
+    imgtype = ".PNG";
 im = cv2.imread("images/" + imgstring + imgtype,cv2.CV_LOAD_IMAGE_COLOR)
+resized = []
+if isPhoto:
+    im = cv2.resize(im, dsize = (0,0), fx = .1, fy = .1, interpolation = cv2.INTER_CUBIC);
 im2 = im.copy();
 out = np.zeros(im.shape,np.uint8)
 gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
-
+if isPhoto:
+    gray = cv2.GaussianBlur(gray,(5,5),0)
+thresh = cv2.adaptiveThreshold(gray,255,adaptiveMethod = cv2.ADAPTIVE_THRESH_GAUSSIAN_C,thresholdType = cv2.THRESH_BINARY,blockSize = 11, C=2)
+#if isPhoto:
+#    gray = thresh
 contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
 print responses
 for cnt in contours:
-    if cv2.contourArea(cnt)>1:
+    if cv2.contourArea(cnt)>15:
         [x,y,w,h] = cv2.boundingRect(cnt)
         cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
         roi = gray[y:y+h,x:x+w]
@@ -65,7 +76,7 @@ for cnt in contours:
         retval, results, neigh_resp, dists = model.find_nearest(roismall, k = 1)
         string = str(chr((results[0][0])))
         cv2.putText(out,string,(x,y+h),0,1,(0,255,0))
-cv2.imshow('orig', im2);
+cv2.imshow('orig', gray);
 cv2.imshow('im',im)
 cv2.imshow('out',out)
 cv2.waitKey(0)
