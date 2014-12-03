63563ccc7aec0fb9b8d7ab872252e44ac1fabe21
Nikhil Garg <nikhil_garg@outlook.com>
2014-12-03 01:37:11 -0600

    added training

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ FEATURESSIZE = {FeatureLabels.PIXEL : pow(SIZESMALLSAVED, 2), FeatureLabels.FFT 
 path = "./trainingdata/";
 MODELTYPES = ["svm", "knn", "nn"];
 # paramaters
-isPhoto = False;
+isPhoto = True;
 mode = Mode.TESTING; #TESTING
 featureLabels = [FeatureLabels.PIXEL, FeatureLabels.FFT, FeatureLabels.PCA];
 featuresdirectory = "";
 modeltype = MODELTYPES[1];
     
-imgname = "1"
+imgname = "photo21cropped"
 #########################
 for i in featureLabels:
     featuresdirectory += str(i);
